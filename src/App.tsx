7ba3a04a852063a64b315d4f7d0d5d377c280b8c
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-13 19:55:08 -0400

    Adjusting dimensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Flex, Box } from "@chakra-ui/react";
 import QRCard from "./components/QRCard";
 
 const App = () => {
   return (
-    <>
-      <QRCard />
-    </>
+    <Flex
+      direction="column"
+      h="100vh"
+      alignItems="center"
+      backgroundColor="blue.100"
+    >
+      <Box m="0.2">
+        <QRCard />
+      </Box>
+    </Flex>
   );
 };
 
--- src/components/QRCard.tsx
+++ src/components/QRCard.tsx
@@ -11,10 +11,10 @@ import QRImage from "../assets/image-qr-code.png";
 
 const QRCard = () => {
   return (
-    <Center>
-      <Card mt="5" maxW="md" borderRadius="lg">
+    <Center h="100vh">
+      <Card w={{ base: "375px", md: "1440px" }} maxW="md" borderRadius="3xl">
         <CardBody>
-          <Image src={QRImage} borderRadius="lg" />
+          <Image src={QRImage} borderRadius="3xl" />
           <Stack mt="5" spacing="4">
             <Heading fontSize={30} textAlign="center">
               {" "}
