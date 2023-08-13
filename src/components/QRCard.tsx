b7339707fc0538be0a0f3951bd88e96c548e2772
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-13 18:14:09 -0400

    Incorporating text elements to card

--- src/components/QRCard.tsx
+++ src/components/QRCard.tsx
@@ -1,17 +1,34 @@
-import { Box, Card, CardBody, Center, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Center,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import QRImage from "../assets/image-qr-code.png";
 
 const QRCard = () => {
   return (
-    <Box>
-      <Center>
-        <Card maxW="lg" borderRadius="lg">
-          <CardBody>
-            <Image src={QRImage} />
-          </CardBody>
-        </Card>
-      </Center>
-    </Box>
+    <Center>
+      <Card mt="5" maxW="md" borderRadius="lg">
+        <CardBody>
+          <Image src={QRImage} borderRadius="lg" />
+          <Stack mt="5" spacing="4">
+            <Heading fontSize={30} textAlign="center">
+              {" "}
+              Improve your front-end skills by building projects
+            </Heading>
+            <Text textAlign="center" fontSize="lg" textColor="gray">
+              Scan the QR code to visit Frontend{" "}
+              <Text> Mentor and take your coding skills to </Text> the next
+              level
+            </Text>
+          </Stack>
+        </CardBody>
+      </Card>
+    </Center>
   );
 };
 
