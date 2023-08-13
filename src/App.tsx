f52abd0109966abd18a69a6bc248e4672c02b673
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-13 01:48:03 -0400

    fix provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <ChakraProvider>
+    <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </ChakraProvider>
+    </>
   );
 }
 
