e81186c6756f36967336787e2b9d60df94a30521
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-13 21:35:50 -0400

    updating base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const REPO_URL = "qr-challenge";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${REPO_URL}/`,
+});
