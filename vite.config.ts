16a0f7857e1e4864ee2960dc65f1c014e439c0b3
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-08-13 21:39:08 -0400

    Removing static build github pages file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const REPO_URL = "qr-challenge";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${REPO_URL}/`,
 });
